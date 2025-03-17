87d7a7d3c565e85b6073239df3aed957a98762d4
annahosanna <annahosanna@gmail.com>
2025-03-17 14:56:23 -0400

    Fix AbstractVerticle

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vertx-tls-experiment
-Start a TLS and non encrpted web server using vertx
+
+## Simple experiment
+
+The goal is to start two verticles at the same time and have one use TLS with a self signed certificate (the certificate is created seperately)
+This will then be added to the httpp3 server, in order to meet the requirement that user should not see a difference in the uri (same pages, same ports etc.) even though its being served from udp rather than tcp
--- src/main/java/example/VertxServer.java
+++ src/main/java/example/VertxServer.java
@@ -1,21 +1,25 @@
 package example;
 
 import io.vertx.core.Future;
-import io.vertx.core.VerticleBase;
+// import io.vertx.core.VerticleBase;
+import io.vertx.core.Vertx;
+import io.vertx.core.*;
 import io.vertx.ext.web.Router;
-import io.vertx.launcher.application.VertxApplication;
+import java.lang.Integer;
+// import io.vertx.launcher.application.VertxApplication;
 
 /*
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class VertxServer extends VerticleBase {
+// https://github.com/hakdogan/IntroduceToEclicpseVert.x/tree/master
+public class VertxServer extends AbstractVerticle {
 
   public static void main(String[] args) {
-    VertxApplication.main(new String[] { Server.class.getName() });
+    // VertxApplication.main(new String[] { Server.class.getName() });
   }
 
   @Override
-  public Future<?> start() throws Exception {
+  public void start(Promise<Void> promise) throws Exception {
     Router router = Router.router(vertx);
 
     String responseText = new String("Just a test");
@@ -25,11 +29,11 @@ public class VertxServer extends VerticleBase {
         routingContext
           .response()
           .putHeader("Content-Type", "text/plain")
-          .putHeader("Content-Length", responseText.length)
+          .putHeader("Content-Length", Integer.toString(responseText.length()))
           .putHeader("Alt-Svc", "\"h3=\":8443")
           .end(responseText);
       });
 
-    return vertx.createHttpServer().requestHandler(router).listen(8080);
+    vertx.createHttpServer().requestHandler(router).listen(8080);
   }
 }
